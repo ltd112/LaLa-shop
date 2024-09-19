0c83355042daea7b74caba3e582c3a4be1c4940d
ltd112 <lethanhdat032755@gmail.com>
2024-09-19 19:22:25 +0700

    Initial commit

---
+++ src/main/java/com/dat/LaLa_shop/LaLaShopApplication.java
@@ -0,0 +1,13 @@
+package com.dat.LaLa_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaLaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaLaShopApplication.class, args);
+	}
+
+}
